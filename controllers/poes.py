ac07c064e3a3efd199be929de39ab21649b50ac3
Rommel David Marcillo Pilay <romdmarc@espol.edu.ec>
2024-11-19 10:13:17 -0500

    grafica

--- controllers/poes.py
+++ controllers/poes.py
@@ -23,6 +23,27 @@ def main():
 
     print(valoresCaudal)
 
+    chart1 = sheet.charts.add()
+    chart1.name = "Pwf vs Q0"
+    chart1.chart_type = "xy_scatter_lines"
+
+    cell_position_1 = sheet.range("D1")
+    chart1.left = cell_position_1.left
+    chart1.top = cell_position_1.top
+    chart1.width = 400
+    chart1.height = 300
+
+    chart1.api[1].SeriesCollection().NewSeries()
+    chart1.api[1].SeriesCollection(1).XValues = valoresCaudal
+    chart1.api[1].SeriesCollection(1).Values = valuesPwf
+    chart1.api[1].SeriesCollection(1).Name = "Relación Pwf vs Q0"
+
+    chart1.api[1].ChartTitle.Text = "Relación Pwf vs Q0"
+    chart1.api[1].Axes(1).HasTitle = True
+    chart1.api[1].Axes(1).AxisTitle.Text = "Q0 (stb/d)"
+    chart1.api[1].Axes(2).HasTitle = True
+    chart1.api[1].Axes(2).AxisTitle.Text = "Pwf (psi)"
+
 @xw.func
 def hello(name):
     return f"Hello {name}!"
